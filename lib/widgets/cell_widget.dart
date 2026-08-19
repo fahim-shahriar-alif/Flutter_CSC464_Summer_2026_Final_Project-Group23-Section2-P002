f439493d5ddbe43194d0427d38aa617bc882cd68
sarwe <sarwer.iub.2022@gmail.com>
2026-08-20 01:07:34 +0600

    feat: add CellWidget for board cells

--- lib/widgets/cell_widget.dart
+++ lib/widgets/cell_widget.dart
@@ -1,1 +1,45 @@
-class CellWidget {}
+import 'package:flutter/material.dart';
+
+class CellWidget extends StatelessWidget {
+  final String value;
+  final VoidCallback onTap;
+  final bool isEnabled;
+
+  const CellWidget({
+    super.key,
+    required this.value,
+    required this.onTap,
+    required this.isEnabled,
+  });
+
+  Color _getTextColor() {
+    if (value == 'X') return Colors.blue.shade700;
+    if (value == 'O') return Colors.red.shade700;
+    return Colors.transparent;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (isEnabled && value.isEmpty) ? onTap : null,
+      child: Container(
+        margin: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.deepPurple, width: 2),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            value,
+            style: TextStyle(
+              fontSize: 52,
+              fontWeight: FontWeight.bold,
+              color: _getTextColor(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
