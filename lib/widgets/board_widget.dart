a31331e22fd76ec7c7cc975cbc08569171e8a050
sarwe <sarwer.iub.2022@gmail.com>
2026-08-20 02:09:50 +0600

    feat: add BoardWidget 3x3 grid

--- lib/widgets/board_widget.dart
+++ lib/widgets/board_widget.dart
@@ -1,1 +1,34 @@
-class BoardWidget {}
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/game_provider.dart';
+import 'cell_widget.dart';
+
+class BoardWidget extends StatelessWidget {
+  const BoardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final gameProvider = context.watch<GameProvider>();
+
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: GridView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          ),
+          itemCount: 9,
+          itemBuilder: (context, index) {
+            return CellWidget(
+              value: gameProvider.board[index],
+              onTap: () => gameProvider.makeMove(index),
+              isEnabled: !gameProvider.isGameOver,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
